088550e9e43376fcec1cd8b31f20c042b9e8d72b
jose blois <joseblois3@gmail.com>
2020-05-02 05:20:23 -0300

    added a player and the pause button works now

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var ctx=null,canvas = null;
 var touches=[];
 var COLORS=['#f00','#0f0','#00f','#fff'];
 var scaleX=1,scaleY=1;
+var player = new Rectangle(95,260,10,10);
 var btnLeft=new Button(10,270,20,20);
 var btnRight=new Button(40,270,20,20);
 var btnShoot=new Button(170,270,20,20);
 var btnPause=new Button(90,0,20,20);
+var pause=false;
+var lastPress=null;
 function Button(x,y,width,height){
     this.x = (x===undefined)?0:x;
     this.y = (y===undefined)?0:y;
@@ -19,17 +22,26 @@ Button.prototype.touch = function(){
     for(var i =0, l = touches.length;i<l;i++){
         if(touches[i]!==null){
             if(this.x < touches[i].x &&
-                    this.x + this.width> tocuhes[i].x&&
+                    this.x + this.width> touches[i].x&&
                     this.y < touches[i].y &&
                     this.y + this.height> touches[i].y){
                         return true;
                     }
-        }
+            }
     }
     return false;
 }
 Button.prototype.stroke = function(ctx){
     ctx.strokeRect(this.x,this.y,this.width,this.height);
+}
+function Rectangle(x,y,width,height){
+    this.x = (x===undefined)?0:x;
+    this.y = (y===undefined)?0:y;
+    this.width = (width===undefined)?0:width;
+    this.height = (height===undefined)?width:height;
+}
+Rectangle.prototype.fill = function(ctx){
+    ctx.fillRect(this.x,this.y,this.width,this.height);
 }
 function enableInputs(){
     canvas.addEventListener('touchstart',function(evt){
@@ -64,10 +76,11 @@ function enableInputs(){
     },false);
     canvas.addEventListener('mousedown',function(evt){
         evt.preventDefault();
+
         var x = ~~((evt.pageX - canvas.offsetLeft)*scaleX);
         var y = ~~((evt.pageY - canvas.offsetTop)*scaleY);
-        
         touches[0] = new Point(x,y);
+        lastPress=1;
 
     },false);
     document.addEventListener('mousemove',function(evt){
@@ -117,22 +130,49 @@ function paint(ctx){
     ctx.fillStyle='#000';
     ctx.fillRect(0,0,canvas.width,canvas.height);
 
-    ctx.fillStyle='#999';
+    ctx.fillStyle='#fff';
     for(var i=0; i < touches.length;i++){
         if(touches[i]){
             ctx.fillStyle=COLORS[i%COLORS.length];
-            ctx.fillRect(touches[i].x-10,touches[i].y-10,20,20);
+            ctx.fillRect(touches[i].x-5,touches[i].y-5,10,10);
             ctx.fillText('ID: '+i+' X: '+touches[i].x+' Y: '+touches[i].y,10,10*i+20);
         }
     }
-    ctx.strokeStyle='#fff';
+    if(pause){
+        ctx.textAlign='center'
+        ctx.fillText('PAUSE',canvas.width/2,canvas.height/2)
+    }
+    ctx.textAlign='left'
+    ctx.strokeStyle='#fff';;
+    ctx.fillStyle='#fff';
+    player.fill(ctx)
     btnLeft.stroke(ctx);
     btnRight.stroke(ctx);
     btnShoot.stroke(ctx);
     btnPause.stroke(ctx);
 }
 function act(){
-
+    if(!pause){
+        if(lastPress===1 &&btnPause.touch()){
+            pause=true;
+            lastPress=null;
+        }
+     if(btnRight.touch()){
+         player.x+=5;
+     }
+     if(btnLeft.touch()){
+         player.x-=5;
+     }
+     if(player.x<0){
+        player.x=0
+     }else if(player.x+player.width>canvas.width){
+         player.x=canvas.width-player.width
+     }
+        }
+    if(lastPress===1&&btnPause.touch()){
+        pause=false;
+        lastPress=null;
+    }
 }
 function init(){
     canvas = document.getElementById('canvas');
