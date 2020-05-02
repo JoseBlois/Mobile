f3f2aa5329be1d7813d8cfb1c86a674f5a65baf0
jose blois <joseblois3@gmail.com>
2020-05-02 02:16:05 -0300

    the canvas gets stretched out and resizes

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 (function(window,undefined){
+    'use strict';
 var ctx=null,canvas = null;
 var touches=[];
 var COLORS=['#f00','#0f0','#00f','#fff'];
-
+var scaleX=1,scaleY=1;
 function enableInputs(){
     canvas.addEventListener('touchstart',function(evt){
         var t = evt.changedTouches;
         for(var i = 0;i<t.length;i++){
-            var x =t[i].pageX - canvas.offsetLeft;
-            var y = t[i].pageY - canvas.offsetTop;
+            var x =~~((t[i].pageX - canvas.offsetLeft)*scaleX);
+            var y = ~~((t[i].pageY - canvas.offsetTop)*scaleY);
             touch[t[i].identifier%100]=new Point (x,y); 
         }
     },false);
@@ -28,25 +29,27 @@ function enableInputs(){
         evt.preventDefault();
         var t = evt.changedTouches;
         for(var i =0;i<t.length;i++){
-            touches[t[i].identifier%100].x = evt.pageX - canvas.offsetLeft;
-            touches[t[i].identifier%100].y = evt.pageY - canvas.offsetTop;
+            if(touches[t[i].identifier%100]){
+            touches[t[i].identifier%100].x = ~~((t[i].pageX - canvas.offsetLeft)*scaleX);
+            touches[t[i].identifier%100].y =  ~~((t[i].pageY - canvas.offsetTop)*scaleY);
+            }
         }
     },false);
     canvas.addEventListener('mousedown',function(evt){
         evt.preventDefault();
-        var x = evt.pageX - canvas.offsetLeft
-        var y = evt.pageY - canvas.offsetTop
+        var x = ~~((evt.pageX - canvas.offsetLeft)*scaleX);
+        var y = ~~((evt.pageY - canvas.offsetTop)*scaleY);
         
         touches[0] = new Point(x,y);
 
     },false);
     document.addEventListener('mousemove',function(evt){
         if(touches[0]){
-            touches[0].x = evt.pageX - canvas.offsetLeft;
-            touches[0].y = evt.pageY - canvas.offsetTop;
+            touches[0].x = ~~((evt.pageX - canvas.offsetLeft)*scaleX);
+            touches[0].y = ~~((evt.pageY - canvas.offsetTop)*scaleY);
         }
     },false);
-    document.addEventListener('mouseup',function(){
+    document.addEventListener('mouseup',function(evt){
         touches[0]=null;
     },false);
 }
@@ -54,6 +57,19 @@ function Point(x,y){
     this.x = x||0;
     this.y = y||0;
 
+}
+function resize(){
+    if(window.innerWidth>window.innerHeight){
+        //Landscape
+        canvas.width = 300;
+        canvas.height = 200;
+    }else{
+        //Portrait
+        canvas.width=200;
+        canvas.height=300;
+    }
+    scaleX = canvas.width/window.innerWidth;
+    scaleY = canvas.height/window.innerHeight;
 }
 function run(){
     window.requestAnimationFrame(run);
@@ -82,13 +98,23 @@ function init(){
     ctx = canvas.getContext('2d');
     canvas.width=300;
     canvas.height=200;
+    canvas.style.position='fixed';
+    canvas.style.top='0';
+    canvas.style.left='0';
+    canvas.style.width='100%';
+    canvas.style.height='100%';
+
+    scaleX = canvas.width/window.innerWidth ;
+    scaleY = canvas.height/window.innerHeight;
 
     // console.log('andaaa')
     enableInputs();
+    resize();
     run();
     // repaint();
 
 }
+window.addEventListener('resize',resize,false);
 window.addEventListener('load',init,false);
 window.requestAnimationFrame=(function(){
     return window.requestAnimationFrame ||
